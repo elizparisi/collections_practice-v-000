80e5a2d1232bb34b7eb24548477c102e585c154d
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-14 01:43:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(num)
+  num.sort
+end
+
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(word)
+  word.sort_by { |word| word.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |words| words[2] = "$"
+  new_array << words
+  end
+  return new_array
+end
+
+def find_a(array)
 
